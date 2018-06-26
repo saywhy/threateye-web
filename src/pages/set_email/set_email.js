47bf2882b97862e932334cd5626116b0790618ae
sun_zhong_shuai@163.com <990882345szs>
2018-06-26 15:43:37 +0800

    更新 首页动画

--- src/pages/set_email/set_email.js
+++ src/pages/set_email/set_email.js
@@ -2,8 +2,18 @@
 app.controller('Set_emailController', ['$scope', '$http', '$state', function ($scope, $http, $state) {
     // 初始化
     $scope.init = function (params) {
-
+        console.log(111);
+        
+        $scope.item ={};
+        $scope.item.encryption = 'no';
+        $scope.item.send = 'no';
     }
+   
+   
 
+    
+    $scope.search = function (params) {
+        console.log($scope.item); 
+    };
     $scope.init();
 }]);
--- src/pages/set_net/set_net.js
+++ src/pages/set_net/set_net.js
@@ -2,8 +2,31 @@
 app.controller('Set_netController', ['$scope', '$http', '$state', function ($scope, $http, $state) {
     // 初始化
     $scope.init = function (params) {
-
+        $scope.net = {
+            type:{}
+        };
+        $scope.statusData = [{
+            num: 0,
+            type: 'eth0'
+        }];
+        $scope.net.statusData = 0;
+        $scope.ip_type = [{
+                num: 0,
+                type: '自动获取'
+            },
+            {
+                num: 1,
+                type: '手动获取'
+            }
+        ];
+        $scope.net.type.ip_type = 0;
+        $scope.net.type.enable = 'no';
     }
 
+    //保存
+    $scope.keep = function (params) {
+        console.log( $scope.net);
+        
+    }
     $scope.init();
 }]);
