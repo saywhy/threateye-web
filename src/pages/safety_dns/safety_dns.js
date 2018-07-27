67f70971198d116d3ec68dc7430b8dc5c2458625
sun_zhong_shuai@163.com <990882345szs>
2018-07-27 16:58:14 +0800

    更新上传文件

--- src/pages/safety_dns/safety_dns.js
+++ src/pages/safety_dns/safety_dns.js
@@ -74,6 +74,108 @@ app.controller('Safety_dnsController', ['$scope', '$http', '$state', function ($
         console.log($scope.dns);
         console.log($scope.searchData);
     };
+    // 下载报表
+    $scope.download = function () {
+        if ($scope.pages.count > 1000) {
+            zeroModal.error('下载数据不能超出1000条！')
+        } else {
+            zeroModal.confirm({
+                content: "确定下载列表吗？",
+                okFn: function () {
+                    $scope.download_list();
+                },
+                cancelFn: function () {}
+            });
+        }
+    };
+    //下载列表
+    $scope.download_list = function () {
+        var tt = new Date().getTime();
+        var url = './yiiapi/investigate/dns-investigation-export';
+        /**
+         * 使用form表单来发送请求
+         * 1.method属性用来设置请求的类型——post还是get
+         * 2.action属性用来设置请求路径。
+         */
+        var form = $("<form>"); //定义一个form表单
+        form.attr("style", "display:none");
+        form.attr("target", "");
+        form.attr("method", "get"); //请求类型
+        form.attr("action", url); //请求地址
+        $("body").append(form); //将表单放置在web中
+        /**
+         * input标签主要用来传递请求所需的参数：
+         * 1.name属性是传递请求所需的参数名.
+         * 2.value属性是传递请求所需的参数值.
+         * 3.当为get类型时，请求所需的参数用input标签来传递，直接写在URL后面是无效的。
+         * 4.当为post类型时，queryString参数直接写在URL后面，formData参数则用input标签传递
+         *       有多少数据则使用多少input标签
+         */
+        var input1 = $("<input>");
+        input1.attr("type", "hidden");
+        input1.attr("name", "host_ip");
+        input1.attr("value", $scope.params_data.host_ip);
+        form.append(input1);
+
+        var input2 = $("<input>");
+        input2.attr("type", "hidden");
+        input2.attr("name", "dns_ip");
+        input2.attr("value", $scope.params_data.dns_ip);
+        form.append(input2);
+
+        var input3 = $("<input>");
+        input3.attr("type", "hidden");
+        input3.attr("name", "domain");
+        input3.attr("value", $scope.params_data.domain);
+        form.append(input3);
+
+        var input4 = $("<input>");
+        input4.attr("type", "hidden");
+        input4.attr("name", "resolve_ip");
+        input4.attr("value", $scope.params_data.resolve_ip);
+        form.append(input4);
+
+        var input5 = $("<input>");
+        input5.attr("type", "hidden");
+        input5.attr("name", "ttl");
+        input5.attr("value", $scope.params_data.ttl);
+        form.append(input5);
+
+        var input6 = $("<input>");
+        input6.attr("type", "hidden");
+        input6.attr("name", "resolve_result");
+        input6.attr("value", $scope.params_data.resolve_result);
+        form.append(input6);
+
+        var input7 = $("<input>");
+        input7.attr("type", "hidden");
+        input7.attr("name", "start_time");
+        input7.attr("value", $scope.params_data.start_time);
+        form.append(input7);
+
+        var input8 = $("<input>");
+        input8.attr("type", "hidden");
+        input8.attr("name", "end_time");
+        input8.attr("value", $scope.params_data.end_time);
+        form.append(input8);
+
+        var input9 = $("<input>");
+        input9.attr("type", "hidden");
+        input9.attr("name", "current_page");
+        input9.attr("value", 0);
+        form.append(input9);
+
+        var input0 = $("<input>");
+        input0.attr("type", "hidden");
+        input0.attr("name", "per_page_count");
+        input0.attr("value", 0);
+        form.append(input0);
+
+        form.submit(); //表单提交
+    };
+
+
+
     // 时间插件
     $scope.timerange = function (params) {
         $('.timerange').daterangepicker({
--- src/pages/set_rule/set_rule.js
+++ src/pages/set_rule/set_rule.js
@@ -1,15 +1,55 @@
 /* Controllers */
 app.controller('Set_ruleController', ['$scope', '$http', '$state', function ($scope, $http, $state) {
     // 初始化
-    
-    
     $scope.init = function (params) {
-        $("#avatval").click(function(){
+        $("#avatval").click(function () {
             $("input[type='file']").trigger('click');
+            $scope.$apply(function(){
+                $('#progress')[0].style='width:0%';
+                $scope.progress_if=false;
+            })
         });
-        $("input[type='file']").change(function(){
+        $("input[type='file']").change(function () {
             $("#avatval").val($(this).val());
         });
+        $scope.progress_if=false;
+    };
+    // 上传文件
+    $scope.uploadPic = function () {
+        $scope.progress_if=true;
+        var form = document.getElementById('upload'),
+            formData = new FormData(form);
+        $.ajax({
+            url: "./yiiapi/rulebase/upload-package",
+            type: "post",
+            data: formData,
+            processData: false, // 告诉jQuery不要去处理发送的数据
+            contentType: false, // 告诉jQuery不要去设置Content-Type请求头
+            xhr: function () {　　　　
+                var xhr = $.ajaxSettings.xhr();　　　　
+                if (xhr.upload) {　　　　　　
+                    xhr.upload.onprogress = function (progress) {
+                        if (progress.lengthComputable) {
+                                $('#progress')[0].style='width:'+ parseInt(progress.loaded / progress.total * 100) +'%';
+                        }
+                    };
+                    xhr.upload.onloadstart = function () {
+                        console.log('started...');
+                    };　　　
+                }
+                return xhr;　
+            },
+            success: function (res) {
+                res = JSON.parse(res)
+                console.log(res);
+                if (res) {
+
+                }
+            },
+            error: function (err) {
+                alert("网络连接失败,稍后重试", err);
+            }
+        })
     };
 
 
